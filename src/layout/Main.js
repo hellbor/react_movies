ca3f6bb8d29a48a1539b1d234bc015dc566db1db
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2025-09-09 21:21:52 +0300

    Categories DONE

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -5,25 +5,48 @@ import Search from "../components/Search.js";
 import './Main.css';
 
 class Main extends React.Component {
-    state = { movies: [] }
+    state = { movies: [], loading:false, type:"all" }
     componentDidMount() {
         fetch('https://omdbapi.com/?apikey=7bf125ca&s=resident_evil')
             .then(response => response.json())
             .then(data => this.setState({ movies: data.Search }))
     }
-    searchMovie = (str) => {
+    searchMovie = (str, type = 'all') => {
         this.setState({ loading: true })
-        fetch(`https://omdbapi.com/?apikey=7bf125ca&s=${str}`)
+        fetch(`https://omdbapi.com/?apikey=7bf125ca&s=${str}${type !== 'all' ? `&type=${type}`:''}`)
             .then(response => response.json())
-            .then(data => this.setState({ movies: data.Search }))
+            .then
+            (
+                data => 
+                {
+                    if(data.Response === "True")this.setState({ movies: data.Search, loading:false });
+                    else this.setState({movies:[], loading:false});
+                }
+            )
     }
+    // searchMovie = (str, type = 'all') =>
+    // {
+    //     this.setState({loading:true})
+    //     fetch(`https://omdbapi.com/?apikey=94dbc433&s=${str.trim()}${type !== 'all' ? `&type=${type}` : ''}`)
+    //     .then(response => response.json())
+    //     .then
+    //     (
+    //         data => 
+    //         {
+    //             if(data.Response === "True")this.setState({movies: data.Search, loading:false});
+    //             else this.setState({movies:[], loading:false});
+    //         }
+    //     )
+        
+    //         // this.setState({loading:false});
+    // }
     render() {
         return (
             <div className="main">
                 <div className="wrap">
                     <Search searchMovie={this.searchMovie} />
                     {
-                        this.state.movies.length ? <MovieList movies={this.state.movies} /> : <Preloader />
+                        !this.state.loading && this.state.movies.length ? <MovieList movies={this.state.movies} /> : <Preloader />
                     }
                 </div>
             </div>
