407cc2e071400a3ac85d928c254d908d88118a01
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2025-09-11 21:57:39 +0300

    Pagination DONE

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -5,46 +5,31 @@ import Search from "../components/Search.js";
 import './Main.css';
 
 class Main extends React.Component {
-    state = { movies: [], loading:false, type:"all" }
+    state = { movies: [], loading:false, type:"all", count:0 }
     componentDidMount() {
         fetch('https://omdbapi.com/?apikey=7bf125ca&s=resident_evil')
             .then(response => response.json())
-            .then(data => this.setState({ movies: data.Search }))
+            .then(data => this.setState({ movies: data.Search, loading:false, count:data.totalResults }))
     }
-    searchMovie = (str, type = 'all') => {
+    searchMovie = (str, type = 'all', page) => {
         this.setState({ loading: true })
-        fetch(`https://omdbapi.com/?apikey=7bf125ca&s=${str}${type !== 'all' ? `&type=${type}`:''}`)
+        fetch(`https://omdbapi.com/?apikey=7bf125ca&s=${str}${type !== 'all' ? `&type=${type}`:''}${`&page=${page}`}`)
             .then(response => response.json())
             .then
             (
                 data => 
                 {
-                    if(data.Response === "True")this.setState({ movies: data.Search, loading:false });
-                    else this.setState({movies:[], loading:false});
+                    if(data.Response === "True")this.setState({ movies: data.Search, loading:false, count:data.totalResults });
+                    else this.setState({movies:[], loading:false, count:data.totalResults});
                 }
             )
     }
-    // searchMovie = (str, type = 'all') =>
-    // {
-    //     this.setState({loading:true})
-    //     fetch(`https://omdbapi.com/?apikey=94dbc433&s=${str.trim()}${type !== 'all' ? `&type=${type}` : ''}`)
-    //     .then(response => response.json())
-    //     .then
-    //     (
-    //         data => 
-    //         {
-    //             if(data.Response === "True")this.setState({movies: data.Search, loading:false});
-    //             else this.setState({movies:[], loading:false});
-    //         }
-    //     )
-        
-    //         // this.setState({loading:false});
-    // }
-    render() {
+    render() 
+    {
         return (
             <div className="main">
                 <div className="wrap">
-                    <Search searchMovie={this.searchMovie} />
+                    <Search searchMovie={this.searchMovie} totalCount={this.state.count} />
                     {
                         !this.state.loading && this.state.movies.length ? <MovieList movies={this.state.movies} /> : <Preloader />
                     }
